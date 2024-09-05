9f4473e361f7c3b92329aae3e065062e7c401542
charlysss-l <charlizemikaelanadela@gmail.com>
2024-09-05 21:40:03 +0800

    Update NavigationBar.tsx

--- src/components/NavigationBar/NavigationBar.tsx
+++ src/components/NavigationBar/NavigationBar.tsx
@@ -11,8 +11,8 @@ export default function NavigationBar() {
                     <li className={style.nav_items}><Link href="/Gin" className={style.nav_link}>Gin</Link></li>
                     <li className={style.nav_items}><Link href="/Brandy" className={style.nav_link}>Brandy</Link></li>
                     <li className={style.nav_items}><Link href="/Mix" className={style.nav_link}>Mix</Link></li>
-                    <li className={style.nav_items}><Link href="/Shop" className={style.nav_link}>Shop</Link></li>
-                    <li className={style.nav_items}><Link href="/LetsConnect" className={style.nav_link}>Let's Connect</Link></li>
+                   { /*<li className={style.nav_items}><Link href="/Shop" className={style.nav_link}>Shop</Link></li>
+                    <li className={style.nav_items}><Link href="/LetsConnect" className={style.nav_link}>Let's Connect</Link></li>*/}
                 </ul>
             </nav>
         </>
